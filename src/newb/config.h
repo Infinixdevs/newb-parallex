47e9d46deb4b6fcb1363125144c6893cd433654a
Infinixdevs <lynxiummcdevs@gmail.com>
2024-08-16 18:53:31 +0200

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -32,7 +32,7 @@
 /* Color correction */
 #define NL_TONEMAP_TYPE 4   // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES, there's more check tone.ap.h
 
-#define NL_CONSTRAST 0.832  // 0.3 low ~ 2.0 high
+#define NL_CONSTRAST 0.932  // 0.3 low ~ 2.0 high
 #define NL_EXPOSURE 1.4  // [toggle] 0.5 dark ~ 3.0 bright
 //#define NL_SATURATION 1.4 // [toggle] 0.0 grayscale ~ 4.0 super saturated
 //#define NL_TINT vec3(1.0,0.75,0.5) // [toggle] color overlay
